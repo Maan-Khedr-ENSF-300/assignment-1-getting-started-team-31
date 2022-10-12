6b87ed0c7e07cf86baacc7b4366987d93984e06a
Sudarshan12D <113817382+Sudarshan12D@users.noreply.github.com>
2022-10-11 23:49:24 -0600

    Update Main File.py

--- Main File.py
+++ Main File.py
@@ -17,91 +17,106 @@ def multiply(x,y):
 def divide(x,y):
     return x // y
 
+def intval(x):
 
-def main ():
+    while True:
+        try:
+            x = int(input ('Enter the first value:'))
+
+
+        except ValueError:
+            print(f'your entered value is not an integer \n try again!')
+            continue
+        else:
+            return (x)
+            break
+
+def main():
     while True:
         
-        first_value = int(intval)(input ('Enter the first value:'))
+        first_value = int(input('Enter the first value:'))
         first_operator = input('write the first operator:')
         second_value = int(input ('Enter the second value:'))
-        second_operator = input ('Enter the second operator:') 
+        second_operator = input('Enter the second operator:') 
         third_value = int(input ('Enter the third value:'))
 
         if((first_operator == '/') and (second_operator == '*')):
             fir = divide(first_value, second_value)
-            print(f'Answer is :', multiply(fir, third_value))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', multiply(fir, third_value))
             
             
         elif((first_operator == '/') and (second_operator == '+')):
             fir = divide(first_value, second_value)
-            print(f'sub:', add(fir, third_value))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', add(fir, third_value))
 
         elif((first_operator == '/') and (second_operator == '-')):
             fir = divide(first_value, second_value)
-            print(f'sub:', subtract(fir, third_value))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', subtract(fir, third_value))
 
         elif((first_operator == '/') and (second_operator == '/')):
             fir = divide(first_value, second_value)
-            print(f'sub:', divide(fir, third_value))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', divide(fir, third_value))
 ##################################################################################
         
         elif((first_operator == '*') and (second_operator == '/')):
             fir = divide(second_value, third_value)
-            print(f'Answer is :', multiply(first_value, fir))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', multiply(first_value, fir))
             
         elif((first_operator == '*') and (second_operator == '+')):
             fir = multiply(first_value, second_value)
-            print(f'sub:', add(fir, third_value))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', add(fir, third_value))
 
         elif((first_operator == '*') and (second_operator == '-')):
             fir = divide(first_value, second_value)
-            print(f'sub:', subtract(fir, third_value))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', subtract(fir, third_value))
 
         elif((first_operator == '*') and (second_operator == '*')):
             fir = multiply(first_value, second_value)
-            print(f'sub:', multiply(fir, third_value))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', multiply(fir, third_value))
 
 ###################################################################################
 
         elif((first_operator == '+') and (second_operator == '/')):
             fir = divide(second_value, third_value)
-            print(f'Answer is :', add(first_value, fir))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', add(first_value, fir))
             
         elif((first_operator == '+') and (second_operator == '+')):
             fir = add(first_value, second_value)
-            print(f'sub:', add(fir, third_value))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', add(fir, third_value))
 
         elif((first_operator == '+') and (second_operator == '-')):
             fir = add(first_value, second_value)
-            print(f'sub:', subtract(fir, third_value))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', subtract(fir, third_value))
 
         elif((first_operator == '+') and (second_operator == '*')):
             fir = multiply(first_value, second_value)
-            print(f'sub:', add(first_value, fir))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', add(first_value, fir))
 
 #################################################################################
         
         elif((first_operator == '-') and (second_operator == '/')):
             fir = divide(second_value, third_value)
-            print(f'Answer is :', subtract(first_value, fir))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', subtract(first_value, fir))
             
         elif((first_operator == '-') and (second_operator == '+')):
-            fir = add(first_value, second_value)
-            print(f'sub:', subtract(fir, third_value))
+            fir = add(second_value, third_value)
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', subtract(first_value, fir))
 
         elif((first_operator == '-') and (second_operator == '-')):
-            fir = add(first_value, second_value)
-            print(f'sub:', subtract(fir, third_value))
+            fir = subtract(first_value, second_value)
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', subtract(fir, third_value))
 
         elif((first_operator == '-') and (second_operator == '*')):
             fir = multiply(first_value, second_value)
-            print(f'sub:', subtract(first_value, fir))
+            print(f'The answer to the expression {first_value} {first_operator} {second_value} {second_operator} {third_value} is:', subtract(first_value, fir))
 
         
         else:
             print(f'wrong')
 
         quit()
+
+
 
 
 
